e48c381abf0f6de5150e81f71faae4a568ddf352
Sergio Martin <sergiomartinc95@gmail.com>
2019-05-03 21:00:02 +0200

    Practica 5 falta escala de grises

--- filtro1.js
+++ filtro1.js
@@ -7,6 +7,16 @@ function main() {
   //-- Acceso al objeto con el canvas
   var canvas = document.getElementById('display');
 
+  //-- Acceso al deslizador
+  deslizador = document.getElementById('deslizador1')
+  deslizador = document.getElementById('deslizador2')
+  deslizador = document.getElementById('deslizador3')
+
+  //-- Valor del deslizador
+  range_value1 = document.getElementById('range_value1')
+  range_value2 = document.getElementById('range_value2')
+  range_value3 = document.getElementById('range_value3')
+
   //-- Se establece como tamaño del canvas el mismo
   //-- que el de la imagen original
   canvas.width = img.width;
@@ -20,19 +30,93 @@ function main() {
   //-- No se han hecho manipulaciones todavia
   ctx.drawImage(img, 0,0);
 
-  //-- Obtener la imagen del canvas en pixeles
-  var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  //-- Funcion de retrollamada del deslizador
+  deslizador1.oninput = () => {
+    //-- Mostrar el nuevo valor del deslizador
+    range_value1.innerHTML = deslizador1.value
 
-  //-- Obtener el array con todos los píxeles
-  var data = imgData.data
+    //-- Situar la imagen original en el canvas
+    //-- No se han hecho manipulaciones todavia
+    ctx.drawImage(img, 0,0);
 
-  //-- Eliminar el canal Rojo:recorrer el array de datos
-  //-- eliminado el canal rojo y dejando el resto igual que
-  //-- estaba
-  for (var i = 0; i < data.length; i+=4) {
-    data[i] = 0; //-- Canal rojo a 0
+    //-- Obtener la imagen del canvas en pixeles
+    var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+    //-- Obtener el array con todos los píxeles
+    var data = imgData.data
+
+    //-- Obtener el umbral de rojo del desliador
+    umbral = deslizador1.value
+
+    //-- Filtrar la imagen según el nuevo umbral
+    for (var i = 0; i < data.length; i+=4) {
+      if (data[i] > umbral)
+        data[i] = umbral;
+    }
+
+    //-- Poner la imagen modificada en el canvas
+    ctx.putImageData(imgData, 0, 0);
   }
 
-  //-- Poner la imagen modificada en el canvas
-  ctx.putImageData(imgData, 0, 0);
+
+
+  //-- Funcion de retrollamada del deslizador
+  deslizador2.oninput = () => {
+    //-- Mostrar el nuevo valor del deslizador
+    range_value2.innerHTML = deslizador2.value
+
+    //-- Situar la imagen original en el canvas
+    //-- No se han hecho manipulaciones todavia
+    ctx.drawImage(img, 0,0);
+
+    //-- Obtener la imagen del canvas en pixeles
+    var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+    //-- Obtener el array con todos los píxeles
+    var data = imgData.data
+
+    //-- Obtener el umbral de verde del desliador
+    umbral = deslizador2.value
+
+    //-- Filtrar la imagen según el nuevo umbral
+    for (var i = 1; i < data.length; i+=4) {
+      if (data[i] > umbral)
+        data[i] = umbral;
+    }
+
+    //-- Poner la imagen modificada en el canvas
+    ctx.putImageData(imgData, 0, 0);
+  }
+
+  //-- Funcion de retrollamada del deslizador
+  deslizador3.oninput = () => {
+    //-- Mostrar el nuevo valor del deslizador
+    range_value3.innerHTML = deslizador3.value
+
+    //-- Situar la imagen original en el canvas
+    //-- No se han hecho manipulaciones todavia
+    ctx.drawImage(img, 0,0);
+
+    //-- Obtener la imagen del canvas en pixeles
+    var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+    //-- Obtener el array con todos los píxeles
+    var data = imgData.data
+
+    //-- Obtener el umbral de verde del desliador
+    umbral = deslizador3.value
+
+    //-- Filtrar la imagen según el nuevo umbral
+    for (var i = 2; i < data.length; i+=4) {
+      if (data[i] > umbral)
+        data[i] = umbral;
+    }
+
+    //-- Poner la imagen modificada en el canvas
+    ctx.putImageData(imgData, 0, 0);
+  }
+
+
+
+
 }
